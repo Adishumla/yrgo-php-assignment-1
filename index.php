4388f79b8a1370be4ba4717ac0f88afdfb3d1c8f
Adishumla <45212964+Adishumla@users.noreply.github.com>
2022-10-20 14:53:13 +0200

    elephant in the room first commit

--- index.php
+++ index.php
@@ -1,1 +1,37 @@
-<?= 'The Elephpent in the Room'; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="styles/grid.css" />
+    <link rel="stylesheet" href="styles/global.css" />
+    <title>Movie Library</title>
+</head>
+<?php //require movies.php
+require 'movies.php';
+
+?>
+
+<body>
+    <header>
+        <h1>Movie Library</h1>
+    </header>
+    <main>
+        <section class="movie-grid">
+            <?php foreach ($movies as $movie => $movieInfo) : ?>
+                <div class="movie">
+                    <img src="<?php echo $movieInfo['photo'] ?>" alt="<?php echo $movie ?>" />
+                    <h2><?php echo $movie ?></h2>
+                    <p><?php echo $movieInfo['director'] ?></p>
+                    <p><?php echo $movieInfo['year'] ?></p>
+                    <p><?php echo $movieInfo['genre'] ?></p>
+                    <p><?php echo $movieInfo['rating'] ?></p>
+                </div>
+            <?php endforeach; ?>
+        </section>
+    </main>
+</body>
+
+</html>
---
+++ movies.php
@@ -0,0 +1,61 @@
+<?php
+
+$movies = [
+    'Blade Runner 2049' => [
+        'director' => 'Ridley Scott',
+        'year' => 1982,
+        'genre' => 'Sci-Fi',
+        'rating' => 8.2,
+        'photo' => 'posters/Blade-Runner-2049.jpeg'
+    ],
+    'The Matrix' => [
+        'director' => 'Andy Wachowski',
+        'year' => 1999,
+        'genre' => 'Sci-Fi',
+        'rating' => 8.7,
+        'photo' => 'posters/The-Matrix.jpeg'
+    ],
+    'E.T. the Extra-Terrestrial' => [
+        'director' => 'Steven Spielberg',
+        'year' => 1982,
+        'genre' => 'Sci-Fi',
+        'rating' => 7.9,
+        'photo' => 'posters/E.T..jpeg'
+    ],
+    'The Thing' => [
+        'director' => 'John Carpenter',
+        'year' => 1982,
+        'genre' => 'Horror',
+        'rating' => 8.2,
+        'photo' => 'posters/The-Thing.jpeg'
+    ],
+    'Inception' => [
+        'director' => 'Christopher Nolan',
+        'year' => 2010,
+        'genre' => 'Sci-Fi',
+        'rating' => 8.8,
+        'photo' => 'posters/Inception.jpeg'
+    ],
+    'Interstellar' => [
+        'director' => 'Christopher Nolan',
+        'year' => 2014,
+        'genre' => 'Sci-Fi',
+        'rating' => 8.6,
+        'photo' => 'posters/Interstellar.jpeg'
+    ],
+    'Kong: Skull Island' => [
+        'director' => 'Jordan Vogt-Roberts',
+        'year' => 2017,
+        'genre' => 'Action',
+        'rating' => 6.7,
+        'photo' => 'posters/Kong-Skull-Island.jpeg'
+    ],
+    'RoboCop' => [
+        'director' => 'Paul Verhoeven',
+        'year' => 1987,
+        'genre' => 'Action',
+        'rating' => 7.5,
+        'photo' => 'posters/RoboCop.jpeg'
+    ],
+
+];
