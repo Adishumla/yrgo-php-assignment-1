2c3736b4ca09f79d8dd1b02ecd3d8abd6f68b8a5
Adishumla <45212964+Adishumla@users.noreply.github.com>
2022-10-25 21:19:43 +0200

    updated grid and added colors

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="styles/grid.css" />
     <link rel="stylesheet" href="styles/global.css" />
+    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="/styles/effect.css">
     <title>Movie Library</title>
 </head>
 <?php //require movies.php
@@ -23,11 +25,11 @@ require 'movies.php';
             <?php foreach ($movies as $movie => $movieInfo) : ?>
                 <div class="movie">
                     <img src="<?php echo $movieInfo['photo'] ?>" alt="<?php echo $movie ?>" />
-                    <h2><?php echo $movie ?></h2>
+                    <!-- <h2><?php echo $movie ?></h2>
                     <p><?php echo $movieInfo['director'] ?></p>
                     <p><?php echo $movieInfo['year'] ?></p>
                     <p><?php echo $movieInfo['genre'] ?></p>
-                    <p><?php echo $movieInfo['rating'] ?></p>
+                    <p><?php echo $movieInfo['rating'] ?></p> -->
                 </div>
             <?php endforeach; ?>
         </section>
